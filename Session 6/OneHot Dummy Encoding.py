15873faceaa37dfb68dfd253619d14c1abeeeb82
Reza <rrawasi@gmail.com>
2019-10-10 22:56:16 -0400

    Update OneHot Dummy Encoding.py
    New version of python has a different style for one hot encoding.

--- Session 6/OneHot Dummy Encoding.py
+++ Session 6/OneHot Dummy Encoding.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
 #------- One hot coding ---------
 from numpy import array
 from numpy import argmax
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer
+import pandas as pd
+
+
 # define example
 sampledata = ['chicken', 'chicken', 'cat', 'chicken', 'dog', 'dog', 'horse', 'chicken', 'dog', 'horse']
 values = array(sampledata)
 print(values)
 
-# binary encode
-onehot_encoder = OneHotEncoder(sparse = False)
-integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+lb = LabelBinarizer()
+lb_results = lb.fit_transform(sampledata)
+lb_results_df = pd.DataFrame(lb_results)
+
 print("one-hot encoded:")
-print(onehot_encoded)
+print(lb_results_df.head())
 
 #------ Dummy coding -------------
 import pandas as pd
@@ -28,18 +24,6 @@ df = pd.DataFrame(data=sampledata, columns=['dummy'])
 dummies = pd.get_dummies(df['dummy'])
 print("dummies:")
 print(dummies)
-
-#------- 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
+ 
 
 
